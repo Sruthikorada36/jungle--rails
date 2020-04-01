d144c9b49e36451ee9c103062af5432a63936300
sruthikorada36 <sruthikorada36@gmail.com>
2020-04-01 17:48:17 +0000

    fixing the bug by formatting the money

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    #user MODEL
     @products = Product.all.order(created_at: :desc)
+    # View
+    # render :index implicitly called by similarity in method's name
   end
 
   def show
